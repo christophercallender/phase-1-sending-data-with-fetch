1cc034dfa790d350d55ddfa9704df136752d6c35
Christopher Callender <christophercallender0000@gmail.com>
2022-05-06 19:04:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+function submitData(userName, userEmail) {
+   let data = { name: userName, email: userEmail };
+   return fetch(
+      'http://localhost:3000/users',
+      /* configurationObject */ {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+         },
+         body: JSON.stringify(data),
+      }
+   )
+      .then((response) => response.json())
+      .then((data) => (document.body.innerHTML = data.id))
+      .catch((error) => (document.body.innerHTML = error.message));
+}
+
+//OR
+
+function configurationObject(data) {
+   return {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+   };
+}
+function submitData(userName, userEmail) {
+   return fetch(
+      'http://localhost:3000/users',
+      configurationObject({ name: userName, email: userEmail })
+   )
+      .then((response) => response.json())
+      .then((data) => (document.body.innerHTML = data.id))
+      .catch((error) => (document.body.innerHTML = error.message));
+}
